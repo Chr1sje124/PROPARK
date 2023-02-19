edb096128ec5ca5a913e5a05fee256b014f8b54b
KeremTekinnn <335631@student.mboutrecht.nl>
2023-02-19 16:47:36 +0100

    a

---
+++ footer.php
@@ -0,0 +1,40 @@
+<footer>
+    <div>
+        <h3>Links</h3>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="quiz.php">Quiz</a></li>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        </ul>
+    </div>
+
+    <div>
+        <h3>Meer Links</h3>
+        <ul>
+            <li><a href="informatie.php">Informatie</a></li>
+            <ul>
+                <li><a href="geschiedenis.php">Geschiedenis</a></li>
+                <li><a href="symptomen.php">Symptomen</a></li>
+                <li><a href="behandeling.php">Behandeling</a></li>
+            </ul>
+        </ul>
+    </div>
+
+    <div>
+        <h3>Contact</h3>
+        <ul>
+            <li>
+                Email: <a href="mailto:jmx@mboutrecht.nl">jmx@mboutrecht.nl</a>
+            </li>
+            <li>Telefoon: 123456789</li>
+        </ul>
+
+        <h3>Font Size</h3>
+        <button onclick="incrementFontSize()">+</button>
+        <button onclick="decrementFontSize()">-</button>
+        <button onclick="resetFontSize()">Reset</button>
+        
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+
+</head>
+
+<body>
+
+    <header>
+        <div class="inner">
+            <div class="logo">
+                <div>
+                    <img src="adhd-removebg-preview.png" alt="logo">
+                </div>
+            </div>
+
+            <nav>
+
+                <li><span><a href="index.html">Home</a></span></li>
+                <li><span><a href="#">Informatie</a></span></li>
+                <li><span><a href="quiz.html">Quiz</a></span></li>
+            </nav>
+        </div>
+        </div>
+    </header>
+
+    <iframe width="693" height="400" src="https://www.youtube.com/embed/t9UmOySsvYY"
+        title="Wat is ADHD? - Yentl legt het uit!" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+    <div class="homepage">
+        <h1>ADHD</h1>
+        <p>Doe hier nu een zelftest </p>
+        <button class="Quizbt"><a href="quiz.html">Quiz</a></button>
+    </div>
+
+</body>
+
+
+</html>
