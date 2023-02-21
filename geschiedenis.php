9a1a70b04f270787b6055882f857b533a09f35d8
Osama Awad <awado251@gamil.com>
2023-02-21 09:02:07 +0100

    o

--- geschiedenis.php
+++ geschiedenis.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Dyslexie - Informatie</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@500&display=swap" rel="stylesheet">
@@ -13,10 +12,19 @@
 
 <body>
   <?php include "nav.php"; ?>
+<div class="Geschiedenis"> 
+  <div class="content">
 
+<p> <span>Geschiedenis</span> De eerste beschrijving van ADHD werd al in 1902 gedaan door de Engelse kinderarts George Still.
+    De term ADD werd in 1980 voor het eerst opgenomen in het handboek dat gebruikt wordt voor de diagnose van psychische stoornissen (de DSM). Bij een wijziging in 1987 werd de naam ADD weer aangepast naar ADHD.</p>
 
+<img src="./img/ADHD-2.jpg" alt="">
+
+      </div>
+  </div>
   <?php include "footer.php" ?>
 
+  
 </body>
 
 </html>
