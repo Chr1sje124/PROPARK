afc1e69da8a45402eeaabad6c28eb29541ba5068
KeremTekinnn <335631@student.mboutrecht.nl>
2023-03-07 09:15:17 +0100

    behandeling

--- behandeling.php
+++ behandeling.php
@@ -12,8 +12,17 @@
 
 <body>
   <?php include "nav.php"; ?>
-
-
+  <div class="behandeling">
+  <h1>Behandeling</h1>
+  <h3>ADHD behandeling</h3><p class="text">Een ADHD behandeling bestaat uit verschillende delen: psycho-educatie, ADHD medicatie, coaching en psychotherapie. Indien je naast ADHD ook last hebt van bijvoorbeeld een angst- of stemmingsstoornis, dan krijg je een aangepaste behandeling. In het geval van ADHD behandeling bij kinderen ontvangen ook de ouders hulp.</p>
+  <h3>Psycho-educatie</h3><p>Psycho-educatie houdt in dat je meer leert over de stoornis en in het bijzonder over het subtype ADHD wat er bij jou of je kind is vastgesteld. Het is voor jou en/of jouw kind en omgeving belangrijk om te begrijpen wat ADHD inhoudt.</p>
+  <h3>ADHD medicatie</h3><p>Er is veel onderzoek gedaan naar effectieve medicatie voor ADHD, voornamelijk bij kinderen, maar ook bij volwassenen. Het gebruik van medicatie zoals Ritalin blijkt te werken. Medicatie wordt dan ook vaak voorgeschreven voor het verminderen van ADHD klachten. Het ‘geneest’ de klachten niet. De medicatie wordt voorgeschreven door een psychiater of gespecialiseerde arts. Het komt veelvuldig voor dat mensen met ADHD zelf al op zoek zijn naar werkende middelen, echter met het gevaar op verslaving.</p>
+<h3>ADHD coaching</h3><p>Coaching is gericht op het leren omgaan met ADHD. Coaching kan individueel en in een groep. Het voordeel van een groep is dat je snel en veel kan leren van lotgenoten en steun ervaart voor jouw eigen problemen. Behalve aandacht voor informatie over ADHD en medicatie wordt onder andere ook gekeken naar agendaplanning, communicatie en omgaan met geld en administratie.</p>
+  <h3>Psychotherapie</h3><p>Bij een aantal mensen met ADHD kan het nodig zijn dat een afzonderlijk psychotherapeutisch traject onderdeel is van de behandeling. Ingrijpende gebeurtenissen die veel impact hebben op het leven en het functioneren, waardoor je zelfvertrouwen bent verloren, kunnen een reden zijn voor deze behandeling.</p>
+  <h3>Voor ouders</h3><p>De behandelingen voor ouders van kinderen met ADHD zijn erop gericht leren om te gaan met het gedrag van jouw kind en met de gevolgen daarvan. Jouw kind brengt veel tijd op school door, dus ook daar zullen gevolgen van ADHD te merken zijn. Daarom is het belangrijk dat de school van jouw kind bij de behandeling betrokken wordt. Maatregelen en afspraken op school moeten goed aansluiten bij wat er thuis gebeurt.
+  Kinderen kunnen de symptomen van ADHD niet of maar zeer beperkt afleren. Een psychosociale behandeling bij kinderen zal zich daarom enerzijds vooral richten op het aanbrengen van (meer) structuur in de omgeving, via het inschakelen van jou als ouder en de betrokken leerkrachten. Anderzijds zullen kinderen aan de slag gaan met het leren van vaardigheden die helpen om beter te leren omgaan met hun eigen gedrag.</p>
+  </div>
+  <img src="./img/AdHd.jfif" alt="" class="image">
   <?php include "footer.php" ?>
 
 </body>
