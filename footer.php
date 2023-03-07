2997d5a3f096996764f23bb802b423b1f1626a97
KeremTekinnn <335631@student.mboutrecht.nl>
2023-03-07 08:42:38 +0100

    Homepage afmaken

--- footer.php
+++ footer.php
@@ -16,12 +16,6 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="informatie.php">Informatie</a></li>
             <li><a href="quiz.php">Quiz</a></li>
-        </ul>
-    </div>
-
-    <div>
-        <h3>Nog Links</h3>
-        <ul>
             <li><a href="geschiedenis.php">Geschiedenis</a></li>
             <li><a href="symptomen.php">Symptomen</a></li>
             <li><a href="behandeling.php">Behandeling</a></li>
--- index.php
+++ index.php
@@ -15,22 +15,28 @@
   <?php include "nav.php"?>
 
   <h1>Wat is Adhd?</h1>
-
+  <div class="uitleg"><p>ADHD staat voor Attention Deficit Hyperactivity Disorder. In het Nederlands spreken we van een hyperactiviteitstoornis of aandachtstekortstoornis.
+     Iemand met ADHD heeft last van concentratie- en aandachtsproblemen en is vaak hyperactief en impulsief. De aandacht ergens niet bij kunnen
+      houden, rondrennen en niet luisteren zijn herkenbaar bij kinderen. Volwassenen maken regelmatig slordigheidsfouten, kunnen zich moeilijk 
+      concentreren of schuiven onrustig heen en weer op hun stoel. Een heleboel mensen herkennen de symptomen en vatten de verzamelnaam ADHD vaak
+      samen als een te vol hoofd. Ze zijn ook veel bezig met uitzoeken van wat er allemaal in het hoofd afspeelt, in plaats van zich bezig te houden met de dingen van dat moment.
+    </p>
+  </div>
   <div class="quizdiv">
-    <h2>Doe hier nu een zelftest!
-
-      <div class = "button"> <button class="button-19" role="button">Quiz</button>
-</div>
-    </h2>
+    <p>Wilt je testen of je ziekte van ADHD heeft?</p>
+    <div class = "button"> <button class="button-19" role="button">Quiz</button></div>
+    <h2>Doe hier nu een zelftest!</h2>
   </div>
 
   <div class="container">
     <iframe width="640" height="390" src="https://www.youtube.com/embed/_cEYVpsSTuE" title="Wat is ADHD?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>
 
+  
   <?php include "footer.php" ?>
 
   <script src="js/nav.js"></script>
 </body>
 
 </html>
+
